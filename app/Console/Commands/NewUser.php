2ef0348b335b3dbc3c0b83448d61be1b887224f5
tankerkiller125 <tankerkiller125@gmail.com>
2016-06-02 17:07:24 -0400

    Working on command to create users from a command.

---
+++ app/Console/Commands/NewUser.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace NewsCMS\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class NewUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'newscms:newuser 
+    {--e|email= : Users email} 
+    {--p|password= : Users password} 
+    {--f|first_name= : Users first name} 
+    {--l|last_name= : Users last name} 
+    {--u|username= : Users username}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new admin user';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $credentials = [];
+        if (!$this->option('email') && !$this->option('password')) {
+            $credentials['email'] = $this->ask('Whats the users email?', null);
+            $credentials['password'] = $this->secret('Whats the users password? (it will not be displayed)', null);
+        } else {
+            $credentials['email'] = $this->option('email');
+            $credentials['password'] = $this->option('password');
+            $credentials['username'] = $this->option('username');
+            $credentials['first_name'] = $this->option('first_name');
+            $credentials['last_name'] = $this->option('last_name');
+        }
+        \Sentinel::register($credentials);
+    }
+}
