89d4a0733fbb0dcc4346b99ea78870c8ed551097
tankerkiller125 <tankerkiller125@gmail.com>
2016-08-19 09:36:02 -0400

    Adding Modules.

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Pingpong\Themes\ThemesServiceProvider::class,
         Cartalyst\Sentinel\Laravel\SentinelServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Nwidart\Modules\LaravelModulesServiceProvider::class,
     ],
 
     /*
@@ -218,6 +219,7 @@ return [
         'Sentinel' => Cartalyst\Sentinel\Laravel\Facades\Sentinel::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Module' => Nwidart\Modules\Facades\Module::class,
     ],
 
 ];
